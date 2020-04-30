56abaaee7a58ee3729efd7c550ad4367d823f1d1
codingYH <yhe10@tufts.edu>
2020-04-30 12:07:15 -0400

    boost speed

--- NFA.java
+++ NFA.java
@@ -114,12 +114,12 @@ public class NFA {
   boolean match(String s, int nthreads) {
     ForkJoinPool pool = new ForkJoinPool(nthreads);
     pool.invoke(new Check(this, s, startState, false));
-//    while(!pool.isTerminated()){
-//      if (Check.found.get() == true){
-//        pool.shutdown();
-//        return Check.found.get();
-//      }
-//    }
+    while(!pool.isQuiescent()){
+      if (Check.found.get() == true){
+        pool.shutdown();
+        return Check.found.get();
+      }
+    }
     pool.shutdown();
     return Check.found.get();
   }
