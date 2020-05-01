b40bf2757a9d65cd88a7668d3a71a0bf13cfb5aa
codingYH <yhe10@tufts.edu>
2020-05-01 15:23:42 -0400

    threads

--- Check.java
+++ Check.java
@@ -1,8 +1,13 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RecursiveAction;
 import java.util.concurrent.RecursiveTask;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLongArray;
 
 public class Check extends RecursiveTask<Boolean> {
     private final NFA nfa;
@@ -63,15 +68,12 @@ public class Check extends RecursiveTask<Boolean> {
                     }
                 }
             }
-            if (!checks.isEmpty()){
-                invokeAll(checks);
-                boolean result = false;
-                for (Check check: checks){
-                    result = result || check.join();
-                }
-                return result;
-            }else
-                return false;
+            invokeAll(checks);
+            boolean result = false;
+            for (Check check: checks){
+                result = result || check.join();
+            }
+            return result;
         }
     }
 }
--- NFA.java
+++ NFA.java
@@ -120,8 +120,10 @@ public class NFA {
     ForkJoinPool pool = new ForkJoinPool(nthreads);
     LinkedBlockingQueue<String> queue = new LinkedBlockingQueue<>();
     queue.add(s+"!"+startState);
-    boolean r = pool.invoke(new Check(this, new LinkedBlockingQueue<>() , queue));
-    pool.shutdown();
-    return r;
+//    Check c = new Check(this, new LinkedBlockingQueue<String>() , queue);
+//    boolean r = pool.invoke(c);
+//    System.out.println(pool.getRunningThreadCount());
+//    return r;
+    return pool.invoke(new Check(this, new LinkedBlockingQueue<>() , queue));
   }
 }
